8fed1d47d622f6c78f2caa796a78e9def825c8f6
Kwonhyukjoon <victorehko12@naver.com>
2020-12-07 16:11:51 +0900

    Update users.js

--- controller/users.js
+++ controller/users.js
@@ -90,9 +90,11 @@ exports.loginUser = async (req, res, next) => {
   const token = jwt.sign({ user_id: user_id }, process.env.ACCESS_TOKEN_SECRET);
   query = "insert into market_token (token, user_id) values (?, ?)";
   data = [token, user_id];
+  let qur = `select nickname from p_user where email = "${email}"`;
   try {
     [result] = await connection.query(query, data);
-    res.status(200).json({ success: true, token: token });
+    [rows] = await connection.query(qur);
+    res.status(200).json({ success: true, token: token, items: rows });
   } catch (e) {
     res.status(500).json();
   }
