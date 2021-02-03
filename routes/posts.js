1e4fc23f5cb0265e0671185cf2bd380ad8712cc1
zzho4662 <zzho4662@naver.com>
2021-02-03 16:14:19 +0900

    이미지 완료

--- routes/posts.js
+++ routes/posts.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const auth = require("../middleware/auth");
 const {
-  updatePost,
-  deletePost,
   uploadNew,
-  getMyPosts,
-  getFriendsPost,
   uploadImage,
-  uploadPhoto,
 } = require("../controller/posts");
 
 const router = express.Router();
 
 router.route("/image").post(auth, uploadImage);
 router.route("/").post(auth, uploadNew);
-router.route("/").get(auth, getMyPosts);
-router.route("/:post_id").put(auth, updatePost);
-router.route("/:post_id").delete(auth, deletePost);
-router.route("/friend").get(auth, getFriendsPost);
 
 module.exports = router;
